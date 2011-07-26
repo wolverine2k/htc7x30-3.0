d705f05aa5fb61704eed18a109e66af18e0b2ab4
Al Viro <viro@zeniv.linux.org.uk>
2011-07-26 04:22:01 -0400

    switch sys_chmod()/sys_fchmod()/sys_fchmodat() to umode_t
    SYSCALLx magic should take care of things, according to Linus...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Conflicts:
    	fs/open.c

--- fs/open.c
+++ fs/open.c
@@ -484,7 +484,7 @@ out:
 	return err;
 }
 
-SYSCALL_DEFINE3(fchmodat, int, dfd, const char __user *, filename, mode_t, mode)
+SYSCALL_DEFINE3(fchmodat, int, dfd, const char __user *, filename, umode_t, mode)
 {
 	struct path path;
 	struct inode *inode;
@@ -517,7 +517,7 @@ out:
 	return error;
 }
 
-SYSCALL_DEFINE2(chmod, const char __user *, filename, mode_t, mode)
+SYSCALL_DEFINE2(chmod, const char __user *, filename, umode_t, mode)
 {
 	return sys_fchmodat(AT_FDCWD, filename, mode);
 }
