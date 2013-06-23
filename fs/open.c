ac22fecf8b25de88794aa956d53f2a4c61671429
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-06-23 19:33:38 +0300

    fs/open: fix merge errors

--- fs/open.c
+++ fs/open.c
@@ -448,39 +448,15 @@ out:
 
 SYSCALL_DEFINE2(fchmod, unsigned int, fd, mode_t, mode)
 {
-	struct inode * inode;
-	struct dentry * dentry;
 	struct file * file;
 	int err = -EBADF;
-	struct iattr newattrs;
 
 	file = fget(fd);
-	if (!file)
-		goto out;
-
-	dentry = file->f_path.dentry;
-	inode = dentry->d_inode;
-
-	audit_inode(NULL, dentry);
-
-	err = mnt_want_write_file(file);
-	if (err)
-		goto out_putf;
-	mutex_lock(&inode->i_mutex);
-	err = security_path_chmod(path, mode);
-	if (err)
-		goto out_unlock;
-	if (mode == (mode_t) -1)
-		mode = inode->i_mode;
-	newattrs.ia_mode = (mode & S_IALLUGO) | (inode->i_mode & ~S_IALLUGO);
-	newattrs.ia_valid = ATTR_MODE | ATTR_CTIME;
-	err = notify_change(dentry, &newattrs);
-out_unlock:
-	mutex_unlock(&inode->i_mutex);
-	mnt_drop_write(file->f_path.mnt);
-out_putf:
-	fput(file);
-out:
+	if (file) {
+		audit_inode(NULL, file->f_path.dentry);
+		err = chmod_common(&file->f_path, mode);
+		fput(file);
+	}
 	return err;
 }
 
