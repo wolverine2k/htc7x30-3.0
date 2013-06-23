d62d2553d5d15bac72149de834d939d9eb3b83a6
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-06-23 19:39:00 +0300

    fs/open: more fixes

--- fs/open.c
+++ fs/open.c
@@ -446,6 +446,28 @@ out:
 	return error;
 }
 
+static int chmod_common(struct path *path, umode_t mode)
+{
+	struct inode *inode = path->dentry->d_inode;
+	struct iattr newattrs;
+	int error;
+
+	error = mnt_want_write(path->mnt);
+	if (error)
+		return error;
+	mutex_lock(&inode->i_mutex);
+	error = security_path_chmod(path, mode);
+	if (error)
+		goto out_unlock;
+	newattrs.ia_mode = (mode & S_IALLUGO) | (inode->i_mode & ~S_IALLUGO);
+	newattrs.ia_valid = ATTR_MODE | ATTR_CTIME;
+	error = notify_change(path->dentry, &newattrs);
+out_unlock:
+	mutex_unlock(&inode->i_mutex);
+	mnt_drop_write(path->mnt);
+	return error;
+}
+
 SYSCALL_DEFINE2(fchmod, unsigned int, fd, mode_t, mode)
 {
 	struct file * file;
